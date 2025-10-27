e3ed4fa0d6354aa223f2aa8ca1b97c3945f65eea
unknown <alberto.polato@studenti.unimi.it>
2025-10-27 13:08:58 +0100

    Deleted unnecessary columns

--- src/clean_data.py
+++ src/clean_data.py
@@ -7,3 +7,7 @@ def main():
     shots = events[events['type'] == 'Shot'].copy()
     passes = events[events['type'] == 'Pass'].copy()
     dribbles = events[events['type'] == 'Dribble'].copy()
+    #Delete unnecessary columns
+    shots = shots[['player_name', 'minute', 'second', 'team_name', 'location', 'shot_outcome','shot_statsbomb_xg', 'shot_end_location']]
+    passes = passes[['player_name', 'minute', 'second', 'team_name', 'location', 'pass_outcome', 'pass_end_location']]
+    dribbles = dribbles[['player_name', 'minute', 'second', 'team_name', 'location', 'dribble_outcome']]
