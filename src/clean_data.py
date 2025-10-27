f603b6bb647160a4a3e0f808c002f13898c93d3d
unknown <alberto.polato@studenti.unimi.it>
2025-10-27 13:17:01 +0100

    Fixed wrong indexes

--- src/clean_data.py
+++ src/clean_data.py
@@ -8,12 +8,15 @@ def main():
     passes = events[events['type'] == 'Pass'].copy()
     dribbles = events[events['type'] == 'Dribble'].copy()
     #Delete unnecessary columns
-    shots = shots[['player_name', 'minute', 'second', 'team_name', 'location', 'shot_outcome','shot_statsbomb_xg', 'shot_end_location']]
-    passes = passes[['player_name', 'minute', 'second', 'team_name', 'location', 'pass_outcome', 'pass_end_location']]
-    dribbles = dribbles[['player_name', 'minute', 'second', 'team_name', 'location', 'dribble_outcome']]
+    shots = shots[['player', 'minute', 'second', 'team', 'location', 'shot_outcome','shot_statsbomb_xg', 'shot_end_location']]
+    passes = passes[['player', 'minute', 'second', 'team', 'location', 'pass_outcome', 'pass_end_location']]
+    dribbles = dribbles[['player', 'minute', 'second', 'team', 'location', 'dribble_outcome']]
     #Save cleaned data
     shots.to_csv("data/cleaned_shots.csv", index=False)
     passes.to_csv("data/cleaned_passes.csv", index=False)
     dribbles.to_csv("data/cleaned_dribbles.csv", index=False)
 
     print("Saved cleaned data for shots, passes, and dribbles.")
+
+if __name__ == "__main__":
+    main()
