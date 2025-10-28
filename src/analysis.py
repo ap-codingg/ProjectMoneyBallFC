1f15e753202ec19e59a97a341b8bcf961bafbe2f
unknown <alberto.polato@studenti.unimi.it>
2025-10-28 14:18:21 +0100

    Added pass accuracy per player and numpy library to deal with players that didn't made a pass

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def main():
     #Load cleaned shots data
@@ -8,5 +9,10 @@ def main():
     #Shot efficiency
     player_shot = shots.groupby('player', as_index=False).agg({
         'shot_statsbomb_xg': 'sum', 'shot_outcome': 'count'})
-    player_shot['shot_efficiency'] = player_shot['shot_statsbomb_xg'] / player_shot['shot_outcome']
+    player_shot['shot_efficiency'] = player_shot['shot_outcome'] - player_shot['shot_statsbomb_xg']
+    #Pass accuracy
+    player_pass = passes.groupby('player', as_index=False).agg({
+        'pass_outcome': lambda x: (x == 'Successful').sum(), 'player': 'count'})
+    player_pass.rename(columns={'player': 'total_passes'}, inplace=True)
+    player_pass['pass_accuracy'] = player_pass['pass_outcome'] / player_pass['total_passes'].replace(0, np.nan)
     
