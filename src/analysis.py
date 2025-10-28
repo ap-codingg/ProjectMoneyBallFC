64981bb723f924f7189a4d317a459bbdeb7c1d72
unknown <alberto.polato@studenti.unimi.it>
2025-10-28 15:10:19 +0100

    Added dribble success rate

--- src/analysis.py
+++ src/analysis.py
@@ -13,13 +13,18 @@ def main():
     #Pass accuracy per player
     player_pass = passes.groupby('player', as_index=False).agg({
         'pass_outcome': lambda x: (x == 'Successful').sum(), 'player': 'count'})
-    player_pass.rename(columns={'player': 'total_passes'}, inplace=True)
+    player_pass.rename(columns= {'player': 'total_passes'}, inplace=True)
     player_pass['pass_accuracy'] = (player_pass['pass_outcome'] / player_pass['total_passes'].replace(0, np.nan)) * 100
     #Progressive pass ratio per player
     passes['forward_distance'] = passes['pass_end_location'].apply(lambda x: x[0]) - passes['location'].apply(lambda x: x[0])
     passes['is_progressive'] = passes['forward_distance'] > 10
     player_progressive = passes.groupby('player', as_index=False).agg(
-        total_passes = (is_progressive := 'count'),
-        progressive_passes = (is_progressive := 'sum')
+        total_passes = (is_progressive:= 'count'),
+        progressive_passes = (is_progressive:= 'sum')
     ).reset_index()
     player_progressive['progressive_pass_ratio'] = (player_progressive['progressive_passes'] / player_progressive['total_passes'].replace(0, np.nan)) * 100
+    #Dribble success rate per player
+    player_dribble = dribbles.groupby('player', as_index=False).agg({
+        'dribble_outcome': lambda x: (x == 'Successful').sum(), 'player': 'count'})
+    player_dribble.rename(columns= {'player': 'total_dribbles'}, inplace=True)
+    player_dribble['dribble_success_rate'] = (player_dribble['dribble_outcome'] / player_dribble['total_dribbles'].replace(0, np.nan)) * 100
